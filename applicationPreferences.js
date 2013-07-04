9b552e5c618325612e2ae5958a590b9669defc8d
Dan Moore <github@mooreds.com>
2013-07-03 20:00:01 -0600

    removed cordova define and attachment to the window object, as plugman does this for us

--- applicationPreferences.js
+++ applicationPreferences.js
@@ -1,4 +1,3 @@
-cordova.define("cordova/plugin/applicationpreferences", function(require, exports, module) {
 	var exec = require("cordova/exec");
 	var AppPreferences = function () {};
 	
@@ -36,11 +35,3 @@ cordova.define("cordova/plugin/applicationpreferences", function(require, export
 
 	var appPreferences = new AppPreferences();
 	module.exports = appPreferences;
-});
-
-if (!window.plugins) {
-    window.plugins = {};
-}
-if (!window.plugins.applicationPreference) {
-    window.plugins.applicationPreference = cordova.require("cordova/plugin/applicationpreferences");
-}
