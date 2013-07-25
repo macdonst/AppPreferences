12aadfac6221eae072c1d0de5fffb30d5a16007d
Dan Moore <github@mooreds.com>
2013-07-25 10:46:37 -0600

    put back in a closure

--- src/ios/www/applicationPreferences.js
+++ src/ios/www/applicationPreferences.js
@@ -1,5 +1,7 @@
-	var exec = require("cordova/exec");
-	var applicationPreferences = function () {};
+(function() {
+  
+function applicationPreferences() {}
+
 applicationPreferences.prototype.get = function(key,success,fail) 
 {
     var args = {};
@@ -15,4 +17,13 @@ applicationPreferences.prototype.set = function(key,value,success,fail)
     cordova.exec(success,fail,"applicationPreferences","setSetting",[args]);
 };
 
+
+if(!window.plugins) {
+    window.plugins = {};
+}
+if ( ! window.plugins.applicationPreferences ) {
+    window.plugins.applicationPreferences = new applicationPreferences();
+}
+
+})();
 
